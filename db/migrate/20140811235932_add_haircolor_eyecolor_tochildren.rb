9d44d9abddb6ecc966bc9e25eb8583e79c1f5c01
benmckellick <benjamn8@hotmail.com>
2014-08-11 20:01:24 -0400

    hair eye color

---
+++ db/migrate/20140811235932_add_haircolor_eyecolor_tochildren.rb
@@ -0,0 +1,4 @@
+class AddHaircolorEyecolorTochildren < ActiveRecord::Migration
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140811225127) do
+ActiveRecord::Schema.define(version: 20140811235932) do
 
   create_table "children", force: true do |t|
     t.text     "firstname"
