9acbe44ca586a27592111a8473c2adacf1fccfd5
benmckellick <benjamn8@hotmail.com>
2014-08-11 20:50:38 -0400

    columns added

--- db/migrate/20140812004233_add_attributes_to_children.rb
+++ db/migrate/20140812004233_add_attributes_to_children.rb
@@ -1,6 +1,6 @@
 class AddAttributesToChildren < ActiveRecord::Migration
   def change
   	add_column :children, :hair_color, :text
-  	add_column :children, :eyecolor, :text
+  	add_column :children, :eye_color, :text
   end
 end
